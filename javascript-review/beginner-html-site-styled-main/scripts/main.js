7e14f9d027fe8f0b2d9260a32ca388385a94d755
Duy Le <94324394+SabreBirdOne@users.noreply.github.com>
2025-11-05 16:55:42 -0500

    Add user name changer

--- javascript-review/beginner-html-site-styled-main/scripts/main.js
+++ javascript-review/beginner-html-site-styled-main/scripts/main.js
@@ -8,3 +8,27 @@ myImg.addEventListener("click", () => {
         myImg.setAttribute("src", "images/firefox-icon.png") 
     }
 });
+
+const buttonChangeUser = document.querySelector("button");
+const header = document.querySelector("h1");
+
+function setUserName() {
+    const newName = prompt("What is the new username?");
+    if (!newName) {
+        setUserName();
+    }
+    else {
+        localStorage.setItem("name", newName);
+        header.textContent = `Mozilla is cool, ${newName}`; 
+    }
+    
+}
+
+if (!localStorage.getItem("name")){
+    setUserName();
+} else {
+    const name = localStorage.getItem("name");
+    header.textContent = `Mozilla is cool, ${name}`; 
+}
+
+buttonChangeUser.addEventListener("click", setUserName);
