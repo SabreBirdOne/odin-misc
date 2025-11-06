09d915fef2e60d6428bb455aeb159e72d07c33fb
Duy Le <94324394+SabreBirdOne@users.noreply.github.com>
2025-11-06 12:21:51 -0500

    Add Book constructor for the exercise

---
+++ javascript-course/object-constructors/constructor-exercise.js
@@ -0,0 +1,25 @@
+// https://www.theodinproject.com/lessons/node-path-javascript-objects-and-object-constructors#exercise
+
+/* 
+Write a constructor for making “Book” objects. We will revisit this in the next project. 
+Your book objects should have the book’s title, author, the number of pages, 
+and whether or not you have read the book.
+
+Put a function into the constructor that can report the book info like so:
+theHobbit.info(); // "The Hobbit by J.R.R. Tolkien, 295 pages, not read yet"
+*/
+
+function Book(title, author, numPages) {
+    this.title = title;
+    this.author = author;
+    this.numPages = numPages;
+    this.isRead = false;
+    this.info = function (){
+        const isReadStr = this.isRead ? "read" : "not read yet"; 
+        return `${this.title} by ${this.author}, ${this.numPages} pages, ` + isReadStr;
+    }
+}
+
+const theHobbit = new Book("The Hobbit", "J.R.R Tolkien", 295);
+theHobbit.info();
+console.log(theHobbit.info());
