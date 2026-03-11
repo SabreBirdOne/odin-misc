51682b7ac9e9df07c1fd89f816a6abad17948279
Duy Le <94324394+SabreBirdOne@users.noreply.github.com>
2026-03-11 15:47:21 -0400

    Add new example to learn-constraint-validation-api

---
+++ learn-constraint-validation-api/script-extending-build-in-validation.js
@@ -0,0 +1,14 @@
+const email = document.getElementById("mail");
+
+email.addEventListener("input", (event) => {
+  // Validate with the built-in constraints
+  email.setCustomValidity("");
+  if (!email.validity.valid) {
+    return;
+  }
+
+  // Extend with a custom constraints
+  if (!email.value.endsWith("@example.com")) {
+    email.setCustomValidity("Please enter an email address of @example.com");
+  }
+});
